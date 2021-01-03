25d9108c132d237c80139fbdf8fd7e4f2ad0aad4
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-01-03 20:28:05 +0530

    Interface checkin.

--- src/main/java/com/eric/sample/function/intrf/Generic_Functional_Intrf.java
+++ src/main/java/com/eric/sample/function/intrf/Generic_Functional_Intrf.java
@@ -1,5 +1,12 @@
 package com.eric.sample.function.intrf;
 
+/**
+ * 
+ * @author Shahnawaz
+ *
+ * @param <T>
+ * @param <U>
+ */
 public interface Generic_Functional_Intrf<T, U> {
 	
 	U apply(T arg);
