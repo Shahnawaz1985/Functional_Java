c3d1fe8100ed766d6d64868cd5f9d97fdde39f53
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-01-04 12:43:51 +0530

    Checkin for Faker full address.

--- src/main/java/com/eric/sample/function/util/FakerTest.java
+++ src/main/java/com/eric/sample/function/util/FakerTest.java
@@ -9,8 +9,11 @@ public class FakerTest {
 		String name = faker.name().fullName();
 		String firstName = faker.name().firstName();
 		String lastName = faker.name().lastName();
+		String address = faker.address().fullAddress();
 		
 		System.out.printf("Generated full name, firstName and lastName %s, %s, %s", name, firstName, lastName);
+		System.out.println();
+		System.out.printf("Generated full address %s ", address);
 	}
 
 }
